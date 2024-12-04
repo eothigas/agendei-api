0679077f70905d524061d7da125865e7d310c515
Thiago <thi16thiago@gmail.com>
2024-12-04 12:59:12 -0300

    Testando servidor

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-
-app.listen(3001, () => {
-    console.log("Servidor rodando na porta: 3001");
+app.listen(3333, () => {
+    console.log("Servidor rodando na porta: 3333");
 });
