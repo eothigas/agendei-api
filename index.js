b726a59071f2dca94033828907f40bc4de4e5b4a
Thiago <thi16thiago@gmail.com>
2024-12-04 13:30:50 -0300

    Testando entrada

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/',(request,response)=>{
+  return response.json({message:'Server is up'});
+})
+
+app.get('/atualizou',(request,response)=>{
+  return response.json({message:'atualizou mesmo!'});
+
+})
+app.post('/teste',(request,response)=>{
+  const {name,date} = request.body;
+
+  return response.json({name,date});
+
+})
+
+app.listen(3333)
