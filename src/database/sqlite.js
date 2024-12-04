851fa9e11c92c10233742753b70906e2b1e7c58c
Thiago <thi16thiago@gmail.com>
2024-12-04 13:12:31 -0300

    Revertendo alterações

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -1,34 +1,21 @@
 import sqlite3 from "sqlite3";
-import path from "path";
 
 const SQLite = sqlite3.verbose();
 
-// Caminho absoluto do banco de dados
-const dbPath = path.resolve("src/database/banco.db");
-
-// Inicialização do banco de dados
-const db = new SQLite.Database(
-    dbPath,
-    SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE,
-    (err) => {
-        if (err) {
-            return console.log("Erro ao conectar com banco: " + err.message);
-        }
-        console.log("Conexão com o banco de dados bem-sucedida!");
-    }
-);
-
-// Função para executar consultas
-function query(command, params = [], method = "all") {
-    return new Promise((resolve, reject) => {
+function query(command, params, method = 'all') {
+    return new Promise(function (resolve, reject) {
         db[method](command, params, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
+            if (error)
+                reject(error)
+            else
                 resolve(result);
-            }
         });
     });
 }
+
+const db = new SQLite.Database("./src/database/banco.db", SQLite.OPEN_READWRITE, (err) => {
+    if (err)
+        return console.log("Erro ao conectar com banco: " + err.message);
+});
 
 export { db, query };
