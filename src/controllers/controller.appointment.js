071dbce9cc45e362bed74884b96b0acfef011df7
Thiago <thi16thiago@gmail.com>
2024-12-05 13:33:06 -0300

    Teste API

--- src/controllers/controller.appointment.js
+++ src/controllers/controller.appointment.js
@@ -3,7 +3,7 @@ import serviceAppointment from "../services/service.appointment.js";
 async function ListarByUser(req, res) {
 
     const id_user = req.id_user;
-    const appointments = await serviceAppointment.Listar(id_user, "", "", "");
+    const appointments = await serviceAppointment.Listar(id_user);
 
     res.status(200).json(appointments);
 }
