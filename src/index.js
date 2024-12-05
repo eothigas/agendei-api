b77f980f90ab33ec59a2d8de3a6bae78dc5055a8
Thiago <thi16thiago@gmail.com>
2024-12-05 10:02:52 -0300

    Refresh API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes.js';  // Presumo que você tenha seu arquivo de rotas
+import router from './routes.js'; // Presumo que você tenha seu arquivo de rotas
 
 const app = express();
 
-// Permite que a API aceite requisições de todas as origens
-// Você pode substituir '*' por um domínio específico para permitir apenas de origens específicas
+// Permite qualquer origem com CORS
 app.use(cors({
-  origin: 'http://18.230.206.19:3001',  // Permite todas as origens (cuidado com a segurança)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Permite métodos HTTP específicos
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Permite cabeçalhos específicos
+  origin: '*', // Permite todas as origens
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permite métodos HTTP específicos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permite cabeçalhos específicos
 }));
 
 // Middleware para permitir requisições com JSON
